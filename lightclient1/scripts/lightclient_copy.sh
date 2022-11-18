fbdc22ec52327830d1b49ee157449504819ec8dc
CemGurhan <cemg@live.co.uk>
2022-11-18 08:41:37 +0000

    added ability to provide arguments for trainer noise and model name to lightclient config script. removed lightclient1 used for local testing

--- lightclient1/scripts/lightclient_copy.sh
+++
@@ -1,7 +0,0 @@
-i=$1
-port=$2
-cd ..
-cp -R -v ./lightclient ./lightclient$i
-cd ./lightclient$i
-rm ModelMetadata
-python scripts/sort_data.py -n $i  
--- lightclient1/version_getter.sh
+++
@@ -1,2 +0,0 @@
-i=$(($1))
-curl `${HOST}/api/services/ml_service/v1/models/getmodel?version=$((i))`
