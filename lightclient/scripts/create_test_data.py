11c87c54d1c2c1c36c9f853c38dad8a58d69b3b7
CemGurhan <cemg@live.co.uk>
2022-11-29 20:34:32 +0000

    made test data sent from LC's random selections from train data

--- lightclient/scripts/create_test_data.py
+++ lightclient/scripts/create_test_data.py
@@ -1,7 +1,19 @@
-with open(f"./models/MNIST28X28/data.csv") as training_data:
-    data = training_data.readlines()
+import random
 
-    with open(f"./test_data/test_data.csv", 'w') as write_file:
-        for row in range(100): # first 100 only
-            write_file.writelines(data[row])
+all_data = []
 
+with open(f"./models/MNIST28X28/data.csv", "r") as training_data:
+    data = list(training_data.readlines())
+    header, data = data[0], data[1:]
+
+    random.shuffle(data)
+
+    all_data.append(header)
+    all_data.append(data[:10])
+
+with open(f"./test_data/test_data.csv", 'w') as write_file:
+    for line in all_data:
+        write_file.writelines(line)
+
+
+# line = '0,0,0,0,0...,n'
