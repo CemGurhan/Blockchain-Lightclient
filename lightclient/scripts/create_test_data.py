e1c88602994af4fbfffdb1bb0165d878bc275dc7
CemGurhan <cemg@live.co.uk>
2022-11-29 07:54:56 +0000

    script to create test data properly now finalized

--- lightclient/scripts/create_test_data.py
+++ lightclient/scripts/create_test_data.py
@@ -2,6 +2,6 @@ with open(f"./models/MNIST28X28/data.csv") as training_data:
     data = training_data.readlines()
 
     with open(f"./test_data/test_data.csv", 'w') as write_file:
-        for row in range(10): # first 10 only
+        for row in range(100): # first 10 only
             write_file.writelines(data[row])
 
